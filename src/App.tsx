960b2bb1de86a7d1d8ed593425d4173d2d2c0d32
zubinshah-design <zubin.mba@gmail.com>
2026-02-16 10:47:17 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/zubinshah-b9fc334c">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
