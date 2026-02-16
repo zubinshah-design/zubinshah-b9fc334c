19c26f4dfd7fca48933874ef0bc6fa8a01224cf3
zubinshah-design <zubin.mba@gmail.com>
2026-02-16 12:15:50 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/zubinshah-b9fc334c/',
   server: {
     host: "::",
     port: 8080,
